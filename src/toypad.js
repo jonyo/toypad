b2e78142d51474ad322ac88c72cf102c026050ed
Jonathan Foote <jf@jonathanfoote.com>
2018-08-02 13:30:08 -0500

    Changing name to color singular for consistency

--- src/toypad.js
+++ src/toypad.js
@@ -1,13 +1,13 @@
 var Device = require('./device.js'),
 	panel = require('./panel.js'),
 	minifig = require('./minifig.js'),
-	colors = require('./colors.js');
+	color = require('./color.js');
 
 var toypad = new Device();
 toypad.connect();
 
 // sample to turn it red
-toypad.updatePanel(panel.ALL, colors.RED, 0.5);
+toypad.updatePanel(panel.ALL, color.RED, 0.5);
 
 toypad.disconnect();
 
