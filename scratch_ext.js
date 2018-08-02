1e0ffb19f72470e2a5abdba993882955f36784e3
Jonathan Foote <jf@jonathanfoote.com>
2018-08-01 22:59:33 -0500

    Adding experimental scratch extension

---
+++ scratch_ext.js
@@ -0,0 +1,119 @@
+new (function() {
+	var device = null;
+	var input = null;
+	var poller = null;
+	var ext = this;
+	var debug = false;
+	var speeds = {
+		SLOW: 1.0,
+		MEDIUM: 0.5,
+		FAST: 0.1
+	};
+	var colors = {
+		RED: 0xff0000,
+		GREEN: 0x00ff00,
+		BLUE: 0x0000ff,
+		PURPLE: 0xff00ff,
+		OFF: 0x000000,
+		WHITE: 0xffffff
+	};
+	var panels = {};
+
+	this.colourUpdateNumber_ = 0;
+
+	ext._deviceConnected = function(dev) {
+		if (device) {
+			return;
+		}
+
+		device = dev;
+		device.open();
+
+		poller = setInterval(
+			function() {
+				input = device.read();
+			},
+			20
+		);
+
+		if (debug) {
+			setInterval(
+				function() {
+					console.log(input);
+				},
+				100
+			);
+		}
+	};
+
+	ext._deviceRemoved = function(dev) {
+		if (device != dev){
+			return;
+		}
+		device = null;
+		stopPolling();
+	};
+
+	function stopPolling() {
+		if (poller) {
+			clearInterval(poller);
+		}
+		poller = null;
+	}
+
+	ext._shutdown = function() {
+		if (poller) {
+			clearInterval(poller);
+		}
+		poller = null;
+
+		if (device) {
+			device.close();
+		}
+		device = null;
+	}
+
+	ext._getStatus = function() {
+		if (!device) {
+			return {
+				status: 1, msg: 'Gamepad disconnected'
+			};
+		}
+		return {
+			status: 2, msg: 'Gamepad connected'
+		};
+	}
+
+	ext.updatePanel = function(panel, color, speed) {
+		if (!device) {
+			return;
+		}
+		speed = speeds[speed];
+		color = colors[color];
+		panel = panels[panel];
+		var data = [
+			this.colourUpdateNumber_ & 0xFF,
+			panel,
+			((1 - speed) * 0xFF) & 0xFF,
+			0x01,
+			(color >> 16) & 0xFF,
+			(color >> 8) & 0xFF,
+			color & 0xFF
+		];
+
+		this.colourUpdateNumber_++;
+		device.write([0x55, 0x08, 0xc2].concat(data));
+	}.bind(ext);
+
+	var descriptor = {
+		blocks: [
+			['w', 'set %m.panel color to %m.color %m.speed', 'updatePanel', 'ALL', 'OFF', 'SLOW']
+		],
+		menus: {
+			panel: ['ALL', 'LEFT', 'RIGHT', 'CENTER'],
+			color: ['OFF', 'RED', 'GREEN', 'BLUE', 'PURPLE', 'WHITE'],
+			speed: ['SLOW', 'MEDIUM', 'FAST']
+		}
+	};
+	ScratchExtensions.register('Toypad', descriptor, ext, {type: 'hid', vendor:0x054c, product:0x0268});
+})();
