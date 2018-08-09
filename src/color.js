db0c80db76fa99a7e589bdcb232b7b54633cdd87
Jonathan Foote <jf@jonathanfoote.com>
2018-08-09 15:21:12 -0500

    Cleaning up color jsdocs

--- src/color.js
+++ src/color.js
@@ -1,22 +1,35 @@
-let names = {};
+const names = {};
 
 class Color {
+	/**
+	 * Initialize a color
+	 * @param {string} name
+	 * @param {number} code HEX code in format 0xRRGGBB so all red would be 0xff0000
+	 * @constructor
+	 */
 	constructor (name, code) {
 		this.name = name;
 		this.code = code;
 	}
 
-	static add (name, code) {
-		var color = new Color(name, code);
+	/**
+	 * Add a color to the list of colors that can be referenced by name
+	 * @param {Color} color
+	 * @example
+	 * // Add NAVY
+	 * Color.add(new Color('NAVY', 0x0000aa));
+	 */
+	static add (color) {
 		names[name] = color;
 	}
 }
 
-Color.add('RED', 0xff0000);
-Color.add('GREEN', 0x00ff00);
-Color.add('BLUE', 0x0000ff);
-Color.add('WHITE', 0xffffff);
-Color.add('OFF', 0x000000);
+// Add the main colors
+Color.add(new Color('RED', 0xff0000));
+Color.add(new Color('GREEN', 0x00ff00));
+Color.add(new Color('BLUE', 0x0000ff));
+Color.add(new Color('WHITE', 0xffffff));
+Color.add(new Color('OFF', 0x000000));
 
 exports.names = names;
 exports.Color = Color;
