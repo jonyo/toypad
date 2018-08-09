71475f57c66886bb6787c86f45252e339722099a
Jonathan Foote <jf@jonathanfoote.com>
2018-08-09 15:29:20 -0500

    Cleaning up action jsdocs and how it works

--- src/action.js
+++ src/action.js
@@ -1,21 +1,33 @@
-let codes = {},
-	names = {};
+const codes = {};
+const names = {};
 
+/**
+ * Class representing actions received for minifig-scan
+ */
 class Action {
+	/**
+	 * Constructor to initialize the action
+	 * @param  {string} name
+	 * @param  {number} code
+	 * @constructor
+	 */
 	constructor (name, code) {
 		this.name = name;
 		this.code = code;
 	}
 
-	static add (name, code) {
-		var action = new Action(name, code);
+	/**
+	 * Add a new action to the list.
+	 * @param {Action} action
+	 */
+	static add (action) {
 		codes[code] = action;
 		names[name] = action;
 	}
 }
 
-Action.add('ADD', 0);
-Action.add('REMOVE', 1);
+Action.add(new Action('ADD', 0));
+Action.add(new Action('REMOVE', 1));
 
 exports.codes = codes;
 exports.names = names;
